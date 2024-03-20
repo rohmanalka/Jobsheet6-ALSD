a5a9c0d1846f6430d9ad27fd8e7b0029fecb6c2a
rohmanalka <rohmanalka06@gmail.com>
2024-03-20 17:22:22 +0700

    jobsheet6

---
+++ jobsheet6/Main.java
@@ -0,0 +1,41 @@
+package jobsheet6;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        DaftarMahasiswaBerprestasi list = new DaftarMahasiswaBerprestasi();
+        Mahasiswa m1 = new Mahasiswa("Nusa", 2017, 25, 3);
+        Mahasiswa m2 = new Mahasiswa("Rara", 2012, 19, 4);
+        Mahasiswa m3 = new Mahasiswa("Dompu", 2018, 19, 3.5);
+        Mahasiswa m4 = new Mahasiswa("Abdul", 2017, 23, 2);
+        Mahasiswa m5 = new Mahasiswa("Ummi", 2019, 21, 3.75);
+
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
+
+        System.out.println("Data Mahasiswa Sebelum Sorting = ");
+        list.tampil();
+
+        System.out.println("Data Mahasiswa Setelah Sorting Desc Berdasarkan Ipk = ");
+        list.bubbleSort();
+        list.tampil();
+
+        System.out.println("Data Mahasiswa setelah Sorting asc berdasar IPK = ");
+        list.selectionSort();
+        list.tampil();
+
+        System.out.println("Data Mahasiswa setelah Sorting asc berdasar IPK = ");
+        list.insertionSort();
+        list.tampil();
+
+        System.out.println("Data Mahasiswa setelah Perubahan desc berdasar IPK = ");
+        list.insertionSort();
+        list.tampil();
+    }
+}
+        
